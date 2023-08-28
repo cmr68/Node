7d1df7ba0dfd11b64b2954d3f72c82110ccdfd3d
Cristina M <cristinamunoz@Air-de-Cristina.technicolor.net>
2023-08-28 23:34:23 +0200

    ok

--- dia3/reto1/reto1.js
+++ dia3/reto1/reto1.js
@@ -1,16 +1,21 @@
-const { request, Router } = require('express');
+// const { request, Router } = require('express');
 const express = require('express');
-const { get } = require('express/lib/response');
+// const { get } = require('express/lib/response');
 const app = express();
 
 app.get("/", function(request, response){
     console.log(request.url);
     console.log(request.method);
     console.log(request.headers["user-agent"]);
+    
     console.log("Petición recibida cliente");
-    // response.send("Petición recibida cliente");
+    response.status(200).json({ok:true, message: "Recibido!"});
+  
+});
 
-    // next();
-})
+app.get("/bye", function(request, response){
+    console.log("Desde el get /bye");
+    response.status(200).json({ok:true, message: "Adios!"});
+});
 
-app.listen(3000)
+app.listen(3000);
